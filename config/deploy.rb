5dc4bae40ceb9c4d7c5b35c032615a79cef9a220
Gonzalo Orsi <gonzaloorsi@gmail.com>
2012-06-09 04:42:42 -0300

    Cuarto

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,19 +21,15 @@ set :use_sudo, false
 set :deploy_via, :copy
 
 
-set :default_environment, {
-  'PATH' => "#{deploy_to}/bin:$PATH",
-  'GEM_HOME' => "#{deploy_to}/gems" 
-}
-
-
 
 set :scm, "git"
 set :repository, "git@github.com:gonzaloorsi/#{application}.git"
 set :branch, "master"
-
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+
+
+
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
